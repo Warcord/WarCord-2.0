68b0cbf230da3a594ca2c75ff9eb52e24691c7bd
GardZock <gardzock.contato@gmail.com>
2022-02-20 19:14:07 -0300

    Initial Files + WoT

---
+++ packages/wargaming/world-of-tanks/src/interfaces/user/result.ts
@@ -0,0 +1,11 @@
+import { WOTUserResolve } from './user-return'
+
+interface QueryResult {
+    status: string,
+    meta: {
+        count: number
+    },
+    data: WOTUserResolve[]
+}
+
+export { QueryResult }
